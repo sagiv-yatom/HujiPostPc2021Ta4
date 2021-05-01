39090fff96b9a283266175d35af528cf2114cc64
sagiv-yatom <sagiv.yatom@cs.huji.ac.il>
2021-05-01 15:07:45 +0300

    implementation of CalculateRootsService file

--- app/src/main/java/exercise/find/roots/CalculateRootsService.java
+++ app/src/main/java/exercise/find/roots/CalculateRootsService.java
@@ -20,26 +20,33 @@ public class CalculateRootsService extends IntentService {
       Log.e("CalculateRootsService", "can't calculate roots for non-positive input" + numberToCalculateRootsFor);
       return;
     }
-    /*
-    TODO:
-     calculate the roots.
-     check the time (using `System.currentTimeMillis()`) and stop calculations if can't find an answer after 20 seconds
-     upon success (found a root, or found that the input number is prime):
-      send broadcast with action "found_roots" and with extras:
-       - "original_number"(long)
-       - "root1"(long)
-       - "root2"(long)
-     upon failure (giving up after 20 seconds without an answer):
-      send broadcast with action "stopped_calculations" and with extras:
-       - "original_number"(long)
-       - "time_until_give_up_seconds"(long) the time we tried calculating
 
-      examples:
-       for input "33", roots are (3, 11)
-       for input "30", roots can be (3, 10) or (2, 15) or other options
-       for input "17", roots are (17, 1)
-       for input "829851628752296034247307144300617649465159", after 20 seconds give up
+    long endTime = timeStartMs + 20000;
+    long i = 2;
+    boolean isRootsFound = false;
+    Intent newIntent = new Intent();
 
-     */
+    while (System.currentTimeMillis() < endTime) {
+      if (numberToCalculateRootsFor % i == 0) {
+        isRootsFound = true;
+        break;
+      }
+      i += 1;
+    }
+
+    if (isRootsFound) {
+      newIntent.setAction("found_roots");
+      newIntent.putExtra("original_number", numberToCalculateRootsFor);
+      newIntent.putExtra("root1", i);
+      newIntent.putExtra("root2", numberToCalculateRootsFor / i);
+      newIntent.putExtra("time_until_calculating", System.currentTimeMillis() - timeStartMs);
+    }
+    else {
+      newIntent.setAction("stopped_calculations");
+      newIntent.putExtra("original_number", numberToCalculateRootsFor);
+      newIntent.putExtra("time_until_give_up_seconds", (System.currentTimeMillis() - timeStartMs)/1000);
+    }
+
+    sendBroadcast(newIntent);
   }
 }
